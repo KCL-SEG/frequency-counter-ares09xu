ad215244c2d3ab5610d6266c3a4f43d9caa52412
Shijia Xu <xushijia0910@outlook.com>
2022-11-24 23:08:22 +0000

    finished

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,11 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        itemStr = str(item)
+        if not itemStr in frequencies:
+            frequencies[itemStr] = 1
+        else:
+            up_frequencies = {itemStr : frequencies[itemStr] + 1}
+            frequencies.update(up_frequencies)
     return frequencies
